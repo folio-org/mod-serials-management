4d00c59e1ba79a39790240632d0086cbc19781c6
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2023-06-13 15:47:32 +0100

    docs: PieceGenerationService (#31)
    Added documentation to piece generation service for future development

--- service/grails-app/services/org/olf/PieceGenerationService.groovy
+++ service/grails-app/services/org/olf/PieceGenerationService.groovy
@@ -28,11 +28,15 @@ public class PieceGenerationService {
     // Time unit * period / 365 - rounded up to next whole number
     Integer minNumberOfYears = Math.ceil(timeUnitValues.get(ruleset?.recurrence?.timeUnit?.value)*Integer.parseInt(ruleset?.recurrence?.period)/365)
 
+    // Establish start and end date
     LocalDate startDate = LocalDate.parse(ruleset?.startDate)
     LocalDate endDate = startDate.plusYears(minNumberOfYears)
+
+    // Convert pattern type i.e year_date to YearDate and grab related recurrence pattern class i.e RecurrencePatternYearDate
     final String formattedPatternType = RGX_PATTERN_TYPE.matcher(ruleset?.recurrence?.rules[0]?.patternType).replaceAll{ match -> match.group(1).toUpperCase() }
     final Class<? extends RecurrencePattern> rc = Class.forName("org.olf.recurrence.recurrencePattern.RecurrencePattern${formattedPatternType.capitalize()}")
 
+    // May need fixing, establish counter for keeping track of time unit for use with ordinal
     Integer currentTimeUnitPeriod = (ruleset?.recurrence?.timeUnit?.value == 'year' && Integer.parseInt(ruleset?.recurrence?.period) > 1)  ? 0 : 1
     LocalDate currentTimeUnit = startDate
 
@@ -43,21 +47,27 @@ public class PieceGenerationService {
       year: ChronoField.YEAR,
     ]
 
+    // Iterate through days from start date to end date
     for (LocalDate date = startDate; date.isBefore(endDate); date = date.plusDays(1)) {
+
+      // If current time unit value does not equal the date within the loop, then the iteration must have changed time unit
+      // I.e if the currentTimeUnit is 2022-03-04 and we're tracking the month field, this will fire once the date changes to the 5th month 
       if(currentTimeUnit.get(getTimeUnit.get(ruleset?.recurrence?.timeUnit?.value)) != date.get(getTimeUnit.get(ruleset?.recurrence?.timeUnit?.value))){
-        println(currentTimeUnit)
+
+        //Increment the specified time unit by one
         currentTimeUnit = currentTimeUnit."plus${ruleset?.recurrence?.timeUnit?.value.capitalize()}s"(1)
-        println(currentTimeUnit)
         currentTimeUnitPeriod ++
       }
 
       if(currentTimeUnitPeriod > Integer.parseInt(ruleset?.recurrence?.period)){
-        println(currentTimeUnitPeriod)
+
+        // If the currrentTimeUnitPeriod has passed the max number of time unit (period) change back to default
         currentTimeUnitPeriod = (ruleset?.recurrence?.timeUnit?.value == 'year' && Integer.parseInt(ruleset?.recurrence?.period) > 1)  ? 0 : 1
       }
       for( Integer i = 0; i<ruleset?.recurrence?.rules?.size(); i++ ){
+
+        // Iterating through recurrence rules, if the ordinal matches current time unit period then it is a valid date
         if(currentTimeUnitPeriod == Integer.parseInt(ruleset?.recurrence?.rules[i]?.ordinal)){
-        
         rc.compareDate(ruleset, date, i) && dates.add(date.toString())
         }
       }
