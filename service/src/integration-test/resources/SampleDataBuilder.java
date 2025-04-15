1b56b10833007d88c1deb497dfa388f901eb72f9
Jack Golding <jack.golding@k-int.com>
2025-04-15 10:19:05 +0100

    feat: Initial Json builder service

---
+++ service/src/integration-test/resources/SampleDataBuilder.java
@@ -0,0 +1,7 @@
+ public class SampleDataBuilder {
+
+    public JsonObject buildRecurrence(String patternType, String period, String issues) {
+        JsonObject recurrence = new JsonObject()
+        return
+    }
+}
