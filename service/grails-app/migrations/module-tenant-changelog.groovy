950ff54b2f57e744d447e093a9f6ada71ca556db
Matt Chaib <matthew.chaib@k-int.co.uk>
2025-07-02 15:33:42 +0100

    chore: add file to tenant changelog list

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -3,4 +3,5 @@ databaseChangeLog = {
   include file: 'create-mod-serials-management.groovy'
   include file: 'update-mod-serials-management-1-1.groovy'
   include file: 'update-mod-serials-management-1-2.groovy'
+  include file: 'add-missing-primary-keys-for-trillium.groovy'
 }
