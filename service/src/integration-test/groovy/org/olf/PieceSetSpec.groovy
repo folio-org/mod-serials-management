299cd11228e892e13de49de467b644a98c7597f3
Matt Chaib <matthew.chaib@k-int.co.uk>
2025-07-15 17:09:23 +0100

    refactor: move multiple omission or combination rule tests and create endpoint specs over to new PieceSetSpec file

--- service/src/integration-test/groovy/org/olf/PieceSetSpec.groovy
+++ service/src/integration-test/groovy/org/olf/PieceSetSpec.groovy
@@ -200,4 +200,225 @@ class PieceSetSpec extends BaseSpec {
     [recurrenceName, recurrenceRule, scenarioDescription, omissionRules, combinationRules] <<
       (getRecurrenceOmissionScenarios() + getRecurrenceCombinationScenarios())
   }
+
+  void "CREATE a predicted pieces using a ruleset and a combination rule"() {
+    when: "We ask the system to create predicted pieces"
+    Map combinationRule = [:]
+    combinationRule.put("rules", [ruleset_data.combination.rules.combine_by_issue_2])
+
+    Map rulesetResponse = doPost("/serials-management/rulesets", [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.day,
+      combination: combinationRule,
+      templateConfig:[
+        templateString: "combination/issue piece"
+      ],
+      owner:[
+        id: serialId
+      ],
+      patternType: "day",
+      startDate: startDate
+    ])
+
+    Map respMap = doPost("/serials-management/predictedPieces/create", [
+      id: rulesetResponse.id,
+      startDate: startDate
+    ])
+
+    then: "The system responds with a list of 365 pieces"
+    respMap?.pieces.size() == 365
+    List combinedItems = respMap?.pieces.findAll(p -> p?.combinationOrigins)
+    combinedItems.size() == 1
+  }
+
+  void "Generate predicted pieces with a ruleset containing a 'day' recurrence rule and multiple 'issue' combination rules"() {
+    Map combinationRule = [:]
+    combinationRule.put("rules", [ruleset_data.combination.rules.combine_by_issue_2, ruleset_data.combination.rules.combine_by_issue_10])
+    when: "We ask the system to generate predicted pieces"
+    Map respMap = doPost("/serials-management/predictedPieces/generate", [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.day,
+      combination: combinationRule,
+      templateConfig:[
+        templateString: "combination/issue piece"
+      ],
+      owner:[
+        id: serialId
+      ],
+      patternType: "day",
+      startDate: startDate
+    ])
+
+    then: "Ensure all combined issues exist"
+    List combinedItems = respMap?.pieces.findAll(p -> p?.combinationOrigins)
+    combinedItems.size() == 2
+    List totalPiecesCombined = []
+    respMap?.pieces.forEach{p -> {
+      if (p?.recurrencePieces) p.recurrencePieces.forEach{rp -> totalPiecesCombined.add(rp)}
+    }}
+    totalPiecesCombined.size() == 4
+  }
+
+  void "Create predicted pieces"() {
+    when: "We ask the system to create a ruleset"
+    Map respMap = doPost("/serials-management/rulesets", [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.yearDate,
+      templateConfig: [ templateString: "123"],
+      owner:[
+        id: serialId
+      ],
+      patternType: "year_date",
+      startDate: startDate
+    ])
+
+    then: "Ensure that response is ok and we have a new ruleset"
+    respMap.id != null
+
+    when: "We ask the system to create a predicted piece set based off the previous ruleset"
+    respMap = doPost("/serials-management/predictedPieces/create", [
+      id: respMap.id,
+      startDate: startDate
+    ])
+
+    then: "Ensure that response is ok and we have a new predicted piece set"
+    respMap.id != null
+  }
+
+  void "CREATE a predicted pieces with a ruleset and an omission rule"() {
+    when: "We ask the system to create predicted pieces"
+    Map omissionRule = [:]
+    omissionRule.put("rules", [ruleset_data.omission.rules.omit_by_month_range_may_to_june])
+    Map rulesetResponse = doPost("/serials-management/rulesets",  [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.day,
+      omission: omissionRule,
+      templateConfig:[
+        templateString: "omission/day piece"
+      ],
+      owner:[
+        id: serialId
+      ],
+      patternType: "day",
+      startDate: startDate
+    ])
+
+    Map respMap = doPost("/serials-management/predictedPieces/create", [
+      id: rulesetResponse.id,
+      startDate: startDate
+    ])
+
+    then: "The system responds with a list of 61 omitted pieces"
+    List omittedItems = respMap?.pieces.findAll(p -> p?.omissionOrigins)
+    omittedItems.size() == 61
+  }
+
+  void "CREATE a predicted pieces with the example ruleset"() {
+    when: "We ask the system to create predicted pieces"
+    Map rulesetResponse = doPost("/serials-management/rulesets", [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.day,
+      templateConfig:[
+        templateString: "omission/day piece"
+      ],
+      owner:[
+        id: serialId
+      ],
+      patternType: "day",
+      startDate: startDate
+    ])
+
+    Map respMap = doPost("/serials-management/predictedPieces/create", [id: rulesetResponse.id, startDate: startDate])
+
+    then: "The system responds with a list of 366 pieces"
+    respMap?.pieces.size() == 366
+  }
+
+  void "Generate predicted pieces with a ruleset containing a 'day' recurrence rule and multiple omission rules"() {
+    when: "We ask the system to generate predicted pieces"
+    Map omissionRule = [:]
+    omissionRule.put("rules", [ruleset_data.omission.rules.omit_by_month_range_may_to_june, ruleset_data.omission.rules.omit_by_first_wednesday_in_january, ruleset_data.omission.rules.omit_by_issue_week_month])
+    Map respMap = doPost("/serials-management/predictedPieces/generate", [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.day,
+      omission: omissionRule,
+      templateConfig:[
+        templateString: "omission/day piece"
+      ],
+      owner:[
+        id: serialId
+      ],
+      patternType: "day",
+      startDate: startDate
+    ])
+
+    then: "Ensure that all omitted pieces exist"
+    List omittedItems = respMap?.pieces.findAll(p -> p?.omissionOrigins)
+    omittedItems.size() == 63
+    def actualDates = omittedItems.collect { it.date.toString() }.sort()
+    def expectedDates = ["2024-01-03", "2024-05-01", "2024-05-02", "2024-05-03", "2024-05-04", "2024-05-05", "2024-05-06", "2024-05-07", "2024-05-08", "2024-05-09", "2024-05-10", "2024-05-11", "2024-05-12", "2024-05-13", "2024-05-14", "2024-05-15", "2024-05-16", "2024-05-17", "2024-05-18", "2024-05-19", "2024-05-20", "2024-05-21", "2024-05-22", "2024-05-23", "2024-05-24", "2024-05-25", "2024-05-26", "2024-05-27", "2024-05-28", "2024-05-29", "2024-05-30", "2024-05-31", "2024-06-01", "2024-06-02", "2024-06-03", "2024-06-04", "2024-06-05", "2024-06-06", "2024-06-07", "2024-06-08", "2024-06-09", "2024-06-10", "2024-06-11", "2024-06-12", "2024-06-13", "2024-06-14", "2024-06-15", "2024-06-16", "2024-06-17", "2024-06-18", "2024-06-19", "2024-06-20", "2024-06-21", "2024-06-22", "2024-06-23", "2024-06-24", "2024-06-25", "2024-06-26", "2024-06-27", "2024-06-28", "2024-06-29", "2024-06-30", "2024-11-01"]
+    assert actualDates == expectedDates
+  }
+
+  void "Generate predicted pieces with a ruleset containing a day recurrence and multiple OVERLAPPING combination rules"() {
+    Map combinationRule = [:]
+    combinationRule.put("rules", [ruleset_data.combination.rules.combine_by_issue_1, ruleset_data.combination.rules.combine_by_issue_2])
+    when: "We ask the system to generate predicted pieces"
+    Map respMap = doPost("/serials-management/predictedPieces/generate", [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.day,
+      combination: combinationRule,
+      templateConfig:[
+        templateString: "combination/issue piece"
+      ],
+      owner:[
+        id: serialId
+      ],
+      patternType: "day",
+      startDate: startDate
+    ])
+
+    then: "Ensure all combined issues exist"
+    List combinedItems = respMap?.pieces.findAll(p -> p?.combinationOrigins)
+    combinedItems.size() == 1
+    List totalPiecesCombined = []
+    respMap?.pieces.forEach{p -> {
+      if (p?.recurrencePieces) p.recurrencePieces.forEach{rp -> totalPiecesCombined.add(rp)}
+    }}
+    totalPiecesCombined.size() == 3
+    def actualDates = combinedItems.collectMany { combinedPiece ->
+      combinedPiece?.recurrencePieces?.collect { recurrencePiece ->
+        recurrencePiece?.date?.toString()
+      }
+    }
+    def expectedDates = ["2024-01-01", "2024-01-02", "2024-01-03"]
+    actualDates == expectedDates
+
+  }
+
+  void "Generate predicted pieces with a ruleset containing a day recurrence rule and multiple OVERLAPPING omission rules"() {
+    when: "We ask the system to generate predicted pieces"
+    Map omissionRule = [:]
+    omissionRule.put("rules", [ruleset_data.omission.rules.omit_by_day_of_month_2nd, ruleset_data.omission.rules.omit_by_month_april])
+    Map respMap = doPost("/serials-management/predictedPieces/generate", [
+      rulesetStatus: ruleset_data.rulesetStatus.active,
+      recurrence: ruleset_data.recurrence.day,
+      omission: omissionRule,
+      templateConfig:[
+        templateString: "omission/day piece"
+      ],
+      owner:[
+        id: serialId
+      ],
+      patternType: "day",
+      startDate: startDate
+    ])
+
+    then: "Ensure that all omitted pieces exist"
+    List omittedItems = respMap?.pieces.findAll(p -> p?.omissionOrigins)
+    omittedItems.size() == 41
+    def actualDates = omittedItems.collect { it.date.toString() }.sort()
+    def expectedDates = ["2024-01-02", "2024-02-02", "2024-03-02", "2024-04-01", "2024-04-02", "2024-04-03", "2024-04-04", "2024-04-05", "2024-04-06", "2024-04-07", "2024-04-08", "2024-04-09", "2024-04-10", "2024-04-11", "2024-04-12", "2024-04-13", "2024-04-14", "2024-04-15", "2024-04-16", "2024-04-17", "2024-04-18", "2024-04-19", "2024-04-20", "2024-04-21", "2024-04-22", "2024-04-23", "2024-04-24", "2024-04-25", "2024-04-26", "2024-04-27", "2024-04-28", "2024-04-29", "2024-04-30", "2024-05-02", "2024-06-02", "2024-07-02", "2024-08-02", "2024-09-02", "2024-10-02", "2024-11-02", "2024-12-02"]
+    assert actualDates == expectedDates
+  }
 }
