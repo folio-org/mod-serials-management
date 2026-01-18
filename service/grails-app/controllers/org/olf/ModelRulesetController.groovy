6c445f4e5565c465bf3f7815ae639817ef1b3436
Jack Golding <94838251+Jack-Golding@users.noreply.github.com>
2026-01-18 23:45:47 +0000

    feat: Added replaceAndDelete endpoint for model rulesets (#194)
    MODSER-141

--- service/grails-app/controllers/org/olf/ModelRulesetController.groovy
+++ service/grails-app/controllers/org/olf/ModelRulesetController.groovy
@@ -1,5 +1,6 @@
 package org.olf
 
+import org.olf.SerialRuleset
 import org.olf.ModelRuleset
 
 import com.k_int.okapi.OkapiTenantAwareController
@@ -50,6 +51,50 @@ class ModelRulesetController extends OkapiTenantAwareController<ModelRulesetCont
       modelRuleset.setSerialRuleset(ruleset)
       modelRuleset.save(failOnError: true)
 
+      respond modelRuleset
+    }
+  }
+
+  @Transactional
+  def replaceAndDelete() {
+    ModelRuleset.withTransaction {
+      def data = getObjectToBind()
+      // Existing ruleset
+      String existingModelRulesetId = params.get("modelRulesetId") 
+      ModelRuleset existingModelRuleset = queryForResource(existingModelRulesetId)
+      // If existing ruleset doesnt exist, return error
+      if (existingModelRuleset == null) {
+        transactionStatus.setRollbackOnly()
+        notFound()
+        return
+      }
+      SerialRuleset ruleset = new SerialRuleset([
+        rulesetNumber: data?.serialRuleset?.rulesetNumber,
+        description: data?.serialRuleset?.description,
+        rulesetStatus: data?.serialRuleset?.rulesetStatus,
+        recurrence: data?.serialRuleset?.recurrence,
+        omission: data?.serialRuleset?.omission,
+        combination: data?.serialRuleset?.combination,
+        templateConfig: data?.serialRuleset?.templateConfig,
+      ])
+
+      // Now instantiate a new ModelRuleset
+      ModelRuleset modelRuleset = new ModelRuleset([
+        name: data?.name,
+        description: data?.description,
+        exampleLabel: data?.exampleLabel,
+        modelRulesetStatus: data?.modelRulesetStatus
+      ])
+
+      // Bind serial ruleset to model ruleset so that owner is set correctly
+      modelRuleset.setSerialRuleset(ruleset)
+      modelRuleset.save(failOnError: true)
+      if(modelRuleset.hasErrors()) {
+        transactionStatus.setRollbackOnly()
+        respond modelRuleset.errors
+      }
+      // Finally delete the predicted piece set if we get this far and respond.
+      deleteResource existingModelRuleset
       respond modelRuleset
     }
   }
